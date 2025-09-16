295d49d3cd7e82df6e092ac789b27db38d9bb2c8
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-16 21:43:07 +0530

    BrandNameNotValidException

---
+++ ProductManagementSystem/src/main/java/in/inxod/pms/globalException/BrandNameNotValidException.java
@@ -0,0 +1,8 @@
+package in.inxod.pms.globalException;
+
+public class BrandNameNotValidException extends RuntimeException{
+
+	public BrandNameNotValidException(String msg) {
+        super(msg);
+    }
+}
