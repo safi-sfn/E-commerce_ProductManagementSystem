d046687f3037cac0ece7ef6524e4a9c6a753f0f5
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-16 21:42:41 +0530

    feat(exception) : productNotFound Exception

---
+++ ProductManagementSystem/src/main/java/in/inxod/pms/globalException/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package in.inxod.pms.globalException;
+
+public class ProductNotFoundException extends RuntimeException{
+	
+
+	public ProductNotFoundException(String message) {
+		super(message);
+	}
+}
