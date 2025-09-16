5fdebcae5b9f2d83d9d7a1f8aab8efcec25a6f31
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-16 21:46:14 +0530

    feat(controller) : adding "/products" and "/product/{id}" endpoint

--- ProductManagementSystem/src/main/java/in/inxod/pms/controller/ProductController.java
+++ ProductManagementSystem/src/main/java/in/inxod/pms/controller/ProductController.java
@@ -7,8 +7,13 @@ import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import in.inxod.pms.dto.ProductDto;
+import in.inxod.pms.globalException.ProductNotFoundException;
 import in.inxod.pms.service.ProductServiceImpl;
 
 @RestController
@@ -49,6 +55,22 @@ public class ProductController {
         
 		ProductDto dto = service.addProduct(productDto);
 		return new ResponseEntity<>(dto, HttpStatus.CREATED);
+	}
+	
+	
+	@GetMapping("/products")
+	public ResponseEntity<Page<ProductDto>> getAllProducts(@PageableDefault(size=10, page=0) Pageable pageble){
+		Page<ProductDto> allProduct = service.getAllProduct(pageble);
+		return new ResponseEntity<>(allProduct,HttpStatus.OK);
+	}
+	
+	@GetMapping("/product/{id}")
+	public ResponseEntity<ProductDto> getProductById(@PathVariable int id){
+		ProductDto productDto= service.getProductById(id);
+		if(productDto == null) {
+			throw new ProductNotFoundException("Product with Id " + id + " does not exist");
+		}
+		return new ResponseEntity<>(productDto, HttpStatus.OK);
 	}
 	
 	
