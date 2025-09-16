1cc6a4e9d2697c6547edf29f2a7ab2be73eb0e1d
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-16 17:57:15 +0530

    feat(controller) : api endpoints for add-product

---
+++ ProductManagementSystem/src/main/java/in/inxod/pms/controller/ProductController.java
@@ -0,0 +1,77 @@
+package in.inxod.pms.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import in.inxod.pms.dto.ProductDto;
+import in.inxod.pms.service.ProductServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+	private static final String UPLOAD_DIR = "uploadsImage/";
+	
+	@Autowired
+	private ProductServiceImpl service;
+	
+	@PostMapping("/add-product")
+	public ResponseEntity<ProductDto> addProduct(
+					@RequestParam("product") String product,
+					@RequestParam("image") MultipartFile imageFile) throws  IOException {
+		// ObjectMApper Convert JSON to Java Object
+		ObjectMapper mapper = new ObjectMapper();
+		ProductDto productDto = mapper.readValue(product, ProductDto.class);
+		
+		 // Image file ko local folder me save karna
+        String fileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
+        Path imagePath = Paths.get(UPLOAD_DIR + fileName);
+        Files.createDirectories(imagePath.getParent());
+        Files.write(imagePath, imageFile.getBytes());
+
+        // DTO me sirf image URL set karna
+        String imageUrl = "http://localhost:2426/inxod/pms/api/" + UPLOAD_DIR + fileName;
+        productDto.setProductImageUrl(imageUrl);
+        
+		ProductDto dto = service.addProduct(productDto);
+		return new ResponseEntity<>(dto, HttpStatus.CREATED);
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
