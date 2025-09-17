f87451c14dea80350a36173f23bad0e583502bdb
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-17 22:45:47 +0530

    global exception handle

--- ProductManagementSystem/src/main/java/in/inxod/pms/globalException/ProductExceptionHandler.java
+++ ProductManagementSystem/src/main/java/in/inxod/pms/globalException/ProductExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +23,38 @@ public class ProductExceptionHandler {
 	        );
 		 return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
      }
+	 
+	 @ExceptionHandler(HttpMessageNotReadableException.class)
+	    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+	        ErrorResponse errorResponse = new ErrorResponse(
+	                HttpStatus.BAD_REQUEST.value(),
+	                "Invalid JSON format in request body",
+	                LocalDateTime.now()
+	                
+	        );
+	        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
+	    }
+
+
+		@ExceptionHandler(BrandNameNotValidException.class)
+		public ResponseEntity<ErrorResponse> handleBrandNameException(BrandNameNotValidException e){
+		 	ErrorResponse errorResponse = new ErrorResponse(
+				 HttpStatus.BAD_REQUEST.value(),
+				 e.getMessage(),
+				 LocalDateTime.now()
+		 	);
+		 	return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		}
+	    @ExceptionHandler(Exception.class)
+	    public ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
+		 ErrorResponse errorResponse = new ErrorResponse(
+	                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+	                "Internal Server Error",
+	                LocalDateTime.now()
+	                
+	        );
+	        return new ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR); 
+	    }
+
 	 
 }
