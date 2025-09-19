3e37f284bd5020d372aa4cb53750c9be66a97351
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-19 22:59:23 +0530

    feat(controller) : add a comments for method

--- ProductManagementSystem/src/main/java/in/inxod/pms/repository/ProductRepository.java
+++ ProductManagementSystem/src/main/java/in/inxod/pms/repository/ProductRepository.java
@@ -11,12 +11,15 @@ import org.springframework.stereotype.Repository;
 
 import in.inxod.pms.model.Product;
 
+//extends JpaRepository to get built-in CRUD operations for Product entity
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	// This method searches for products by name using a custom query
 	  @Query("SELECT p FROM Product p WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :keyword, '%'))")
 	    List<Product> searchByName(@Param("keyword") String keyword);
 
+	   // This method searches for products within a specific price range
 	    @Query("select  p from Product p where p.productPrice between :min and :max")
 	    Page<Product> searchByPriceRange(@Param("min") double min, @Param("max") double max, Pageable pageable);
 }
